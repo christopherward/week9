efaeefe52eda4aa4b942cc7d8ea8f56d7b6794ab
christopherward <christopher.ward5@gmail.com>
2015-03-04 20:17:54 -0600

    HW 6 Solution
    Bubble Sort solution, 3.4.15

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,21 @@ import random
 
 def sort(items):
   # 1. TO DO: Implement a "bubble sort" routine here
-  pass
+  swap = -1
+  i = -1
+
+  #while-loop conditions determine whether complete pass was made without a swap
+  while swap != 0 or i != (len(items) - 1):
+    i = 0
+    swap = 0
+    #while-loop condition determines whether two adjacent values need to be swapped
+    while i < (len(items) - 1):
+      if items[i] > items[i+1]:
+        items[i], items[i+1] = items[i+1], items[i]
+        swap = 1
+      i += 1
+  return items
+
 
 
 numbers = list(range(10))
@@ -13,4 +27,4 @@ print("The list was sorted correctly!")
 
 # 2. Change this print statement to display the complexity category.
 #    Refer to the cheat sheet in week9-class for examples.
-print("This algorithm is classified as: O(....)")
+print("This algorithm is classified as: O(n^2)")
